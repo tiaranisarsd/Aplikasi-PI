173425684fdd93b862043dfb455c8aaecf2d5a89
Tiaranisa Rabi'ah Sridewi <160818043+tiaranisarsd@users.noreply.github.com>
2024-09-20 15:18:17 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('kmic_db', 'root', '', {
+const db = new Sequelize('kmic_db', 'root', 'Limadara150195,', {
     host: 'localhost',
     dialect: 'mysql'
 });
